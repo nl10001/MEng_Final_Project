f3de13358a45ac9141ad0fa69af5f0b7470eff07
nl10001 <98903190+nl10001@users.noreply.github.com>
2022-11-18 17:07:44 +0000

    updating basic_commands with odom tested functions

--- my_demo/scripts/basic_commands.py
+++ my_demo/scripts/basic_commands.py
@@ -4,14 +4,29 @@ import rospy
 import numpy as np
 import time
 from geometry_msgs.msg import Twist
-from std_msgs.msg import Float32
+from std_msgs.msg import Float32, Empty
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Imu
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
+
 
 # Class which can be used to issue basic commands to tb3 such as move or turn
 class BasicCommands:
 	def __init__(self):
 		self.twist_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+		self.odom_pub = rospy.Publisher('/odom', Odometry, queue_size=10)
+		self.reset_odom = rospy.Publisher('/reset', Empty, queue_size=10)
+		rospy.Subscriber('/odom', Odometry, self.get_odom)
 		self.vel = Twist()
+		self.odom = Odometry()
+		self.yaw = 0
 		self.rate = rospy.Rate(10)
+
+	def get_odom(self, msg):
+		self.odom = msg
+		orientation_q = msg.pose.pose.orientation
+		orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+		(roll, pitch, self.yaw) = euler_from_quaternion(orientation_list)
 
 	def Move(self, linear_vel, distance):
 			current_dist = 0
@@ -55,4 +70,44 @@ class BasicCommands:
 			self.vel.angular.z = 0
 			self.twist_pub.publish(self.vel)
 			self.rate.sleep()
+
+	def test_move(self, linear_vel, distance):
+		curr_x = self.odom.pose.pose.position.x
+		curr_y = self.odom.pose.pose.position.y
+		current_pos = 0
+		while current_pos < distance:
+			current_pos = np.sqrt(pow(self.odom.pose.pose.position.x - curr_x, 2) + pow(self.odom.pose.pose.position.y - curr_y, 2))
+			self.vel.linear.x = linear_vel
+			self.twist_pub.publish(self.vel)
+			self.rate.sleep()
+			
+		self.vel.linear.x = 0
+		self.twist_pub.publish(self.vel)
+		self.rate.sleep()
+
+	def test_turn(self, angular_vel, degrees, direction):
+		
+		#self.reset_odom.publish(Empty())
+		start_yaw = self.yaw
+		angle = np.radians(degrees)
+		current_angle = 0
+		if direction == "left":
+			self.vel.angular.z = angular_vel
+		elif direction == "right":
+			self.vel.angular.z = -angular_vel
+		else:
+			rospy.loginfo("Default direction used, none specified or spelling error")
+			self.vel.angular.z = angular_vel
+		while current_angle < angle:
+		
+			self.twist_pub.publish(self.vel)
+			current_angle = self.yaw - start_yaw
+			self.rate.sleep()
+					
+		self.vel.angular.z = 0
+		self.twist_pub.publish(self.vel)
+		self.rate.sleep()
+
+
+		
 
